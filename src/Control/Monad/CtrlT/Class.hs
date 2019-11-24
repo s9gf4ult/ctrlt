d559360a0440ba64b57492510820452f8b423bda
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-11-24 22:42:43 +0500

    fix

--- src/Control/Monad/CtrlT/Class.hs
+++ src/Control/Monad/CtrlT/Class.hs
@@ -2,6 +2,7 @@ module Control.Monad.CtrlT.Class where
 
 import           Control.Monad.Catch
 import           Control.Monad.Cont
+import           Control.Monad.Except
 
 class Phoenix (c :: k -> * -> (* -> *) -> * -> *) (m :: * -> *) where
   type Dust c a :: *
@@ -10,14 +11,16 @@ class Phoenix (c :: k -> * -> (* -> *) -> * -> *) (m :: * -> *) where
     -> c t r m a
   reborn  :: m (Dust c a) -> c s r m a
 
-class ForallCC (c :: k -> * -> (* -> *) -> * -> *) where
-  forallCC
+class MonadContA (c :: k -> * -> (* -> *) -> * -> *) where
+  callCCA
     :: ((forall b. a -> c s r m b) -> c s r m a)
     -> c s r m a
 
 type MonadThrowC c m = forall s r. MonadThrow (c s r m)
 
 type MonadContC c m = forall s r. MonadCont (c s r m)
+
+type MonadErrorC e c m = forall s r. MonadError e (c s r m)
 
 type Eval c m a = forall s. c s (Dust c a) m a -> m a
 
@@ -31,12 +34,31 @@ indexedCatch ma handler = burnWith $ \flame -> do
   catch (flame ma) (flame . handler)
 {-# INLINE indexedCatch #-}
 
+data ErrorReason er ex
+  = Err er
+  | Exc ex
+
+tryCatch :: (MonadError e m) => m a -> m (Either e a)
+tryCatch ma = catchError (Right <$> ma) (return . Left)
+
+-- indexedCatchError
+--   :: forall c t r m a e x
+--   .  (Phoenix c m, MonadCatch m, MonadErrorC e c m, Exception x)
+--   => (forall s. c s (Dust c a) m a)
+--   -> (forall q. ErrorReason e x -> c q (Dust c a) m a)
+--   -> c t r m a
+-- indexedCatchError ma handler = burnWith $ \flame -> mask $ \unmask -> do
+--   res <- try $ unmask $ flame $ tryCatch ma
+--   case res of
+--     Left exc -> unmask $ flame $ handler $ Exc exc
+--     Right dust ->
+
 indexedCatchAll
-    :: forall c t r m a
-    .  (Phoenix c m, MonadCatch m)
-    => (forall s. c s (Dust c a) m a)
-    -> (forall q. SomeException -> c q (Dust c a) m a)
-    -> c t r m a
+  :: forall c t r m a
+  .  (Phoenix c m, MonadCatch m)
+  => (forall s. c s (Dust c a) m a)
+  -> (forall q. SomeException -> c q (Dust c a) m a)
+  -> c t r m a
 indexedCatchAll = indexedCatch
 {-# INLINE indexedCatchAll #-}
 
